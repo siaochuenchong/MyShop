b94df3b24fd28ad022485ab65b32863d32e40514
Siao Chuen <siaochuenchong@hyperqb.com>
2020-01-29 18:02:54 +0800

    Added Entity Framework

--- MyShop/MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop/MyShop.WebUI/Controllers/HomeController.cs
@@ -1,15 +1,45 @@
-﻿using System;
+﻿using MyShop.WebUI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace MyShop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        public class ABC
+        {
+            public int Total { get; set; } = 155;
+            public List<SubList> Subs { get; set; } = new List<SubList>()
+            {
+                new SubList()
+                {
+                     id = 1,
+                     Name ="Saya"
+                },
+                new SubList()
+                {
+                    id = 2,
+                    Name = "AKU",
+                }
+            };
+            
+        }
+        public class SubList
+        {
+            public int id { get; set; }
+            public String Name { get; set; }
+        }
         public ActionResult Index()
         {
+            ABC model = new ABC();
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            string jsonData = js.Serialize(model);
+
             return View();
         }
 
